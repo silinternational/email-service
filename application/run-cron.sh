22a94eb087cc97f888e3692773bd1454886085e8
Phillip Shipley <phillip.shipley@gmail.com>
2017-11-27 15:29:32 -0500

    revert cron env load to test

--- application/run-cron.sh
+++ application/run-cron.sh
@@ -18,7 +18,16 @@ chown -R www-data:www-data \
 runny /data/yii migrate --interactive=0
 
 # Dump env to a file
-env >> /etc/environment
+touch /etc/cron.d/email
+env | while read line ; do
+  echo "$line" >> /etc/cron.d/email
+done
+
+# Add env vars to idp-cron to make available to scripts
+cat /etc/cron.d/email-cron >> /etc/cron.d/email
+
+# Remove original cron file without env vars
+rm -f /etc/cron.d/email-cron
 
 # Start cron daemon
 cron -f
