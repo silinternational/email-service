bded873ebdab1dc0b38840cd4008f401891cb244
Matt Henderson <matt_henderson@sil.org>
2018-01-25 14:03:49 -0500

    Fix indentation.

--- application/common/mail/layouts/html.php
+++ application/common/mail/layouts/html.php
@@ -14,24 +14,24 @@ use Sil\PhpEnv\Env;
     $maxWidth = Env::get('EMAIL_MAX_WIDTH', '600px');
     ?>
     <div style="padding: 25px 15px;">
-      <div style="margin-left: auto; margin-right: auto; max-width: <?= $maxWidth ?>;">
-    <header>
-        <table style="background-color: <?= $brandColor ?>; width: 100%">
-            <tr>
-                <td>
-                    <img src="<?= $logo ?>" style="max-height: 4em; vertical-align: middle">
-                </td>
-            </tr>
-        </table>
-    </header>
+        <div style="margin-left: auto; margin-right: auto; max-width: <?= $maxWidth ?>;">
+            <header>
+                <table style="background-color: <?= $brandColor ?>; width: 100%">
+                    <tr>
+                        <td>
+                            <img src="<?= $logo ?>" style="max-height: 4em; vertical-align: middle">
+                        </td>
+                    </tr>
+                </table>
+            </header>
 
-    <div style="border-bottom: 1px solid #d3d3d3; padding: 10px;">
-        <?php
-        /* @var $content string email contents */
-        ?>
-        <?= $content ?>
-    </div>
-      </div>
+            <div style="border-bottom: 1px solid #d3d3d3; padding: 10px;">
+                <?php
+                /* @var $content string email contents */
+                ?>
+                <?= $content ?>
+            </div>
+        </div>
     </div>
 </body>
 </html>
